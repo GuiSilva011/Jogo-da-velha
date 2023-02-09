a1080dcbc1a82222f1b05c8ec200716eb0cab144
GuiSilva011git config --global user.email guilherne1207@gmail.com <guilherne1207@gmail.com>
2023-02-09 19:42:55 -0300

    Criado componente de icone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import styles from './Header.module.css'
-
+import Icon from  '../icon/Icon'
 import Title from '../title/Title'
 import Subtitle from '../subtitle/Subtitle'
 
@@ -8,6 +8,9 @@ function Header(){
      <div className={styles.header}>
         <Title> Jogo da Velha</Title>
         <Subtitle> Criado por Guilherme Alexandre </Subtitle> 
+        <div className={styles.iconContent}>
+            <Icon iconName="github" size='30px' link='https://github.com/GuiSilva011'/>
+        </div>
      </div>
     )
 

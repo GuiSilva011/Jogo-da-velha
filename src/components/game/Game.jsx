bf7965b6c8fa239e59f8d48aff2c79068b8dca02
GuiSilva011git config --global user.email guilherne1207@gmail.com <guilherne1207@gmail.com>
2023-02-15 19:28:57 -0300

    Criado a lógica do jogo

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -2,24 +2,48 @@ import {useState} from 'react'
 
 import styles from './Game.module.css'
 import GameOption from '../gameOption/GameOption'
-
+import Icon from '../icon/Icon'
 
 
 function Game(){
     const [gameState, setGameState] = useState(Array(9).fill(0))
+    const [currentPlayer, setCurrentPlayer] = useState(1)
 
-    console.log(gameState)
+    const handleClick = (pos) => {
+        if (gameState[pos] === 0) {
+            let newGameState = [...gameState]
+            newGameState[pos] = currentPlayer
+            setCurrentPlayer(currentPlayer * -1)
+            setGameState(newGameState)
+        }
+       
+    }
+
+    
     return(
-    <div className={styles.game}>
+     <div className={styles.gameContent}>
+      <div className={styles.game}>
         {
             gameState.map((value, pos)=>
             <GameOption 
             key={`game-option-pos-${pos}`}
-            status = {value} />
+            status = {value}
+            onClick={() => handleClick(pos)} />
             )
         }
    
     </div>
+    <div className={styles.gameInfo}>
+        <h4>Proximo a jogar:</h4>
+        {
+            currentPlayer === 1 && <Icon iconName='circle' />
+        }
+        {
+             currentPlayer === -1 && <Icon iconName='x' />
+        }
+    </div>
+        </div>
+    
     )
 }
 export default Game
--- src/components/gameOption/GameOption.jsx
+++ src/components/gameOption/GameOption.jsx
@@ -3,10 +3,10 @@ import Icon from '../icon/Icon'
 
 const GameIcon = ({iconName}) => <Icon iconName = { iconName } size = '25px' />
 
-function GameOption({ status }){
+function GameOption({ status, onClick }){
     return(
     
-        <div className={styles.gameOption}>
+        <div className={styles.gameOption} onClick={ onClick }>
             {
                 status === 1 &&  <Icon iconName= "circle"  />
             }
